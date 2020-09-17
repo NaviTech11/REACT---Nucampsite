580f185019fe1c157706541fddecdef86c1ec6e9
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-17 10:19:06 -0500

    Add more routing

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
 import './Directory.css'
 import { Link } from 'react-router-dom';
 
@@ -30,11 +30,20 @@ function Directory(props) {
 
         return (
             <div className="container">
-                <div className="row">
-                    {directory}
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
                 </div>
-               
             </div>
+            <div className="row">
+                {directory}
+            </div>
+        </div>
         );
     
 }
